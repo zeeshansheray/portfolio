89a9d88fb706727f765444375befb8776d4a7800
Zeeshan <zeeshan.ali@isystematic.com.pk>
2021-07-02 18:24:22 +0500

    portfoilio background

---
+++ src/pages/main.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+import TypeWriter from 'typewriter-effect';
+
+import '../assets/css/main.scss'
+
+import sample from '../background.mp4';
+
+export default function main() {
+    return (
+        <div id="main" className="">
+            <section>
+                <div className="row video">
+                    <video 
+                    loop 
+                    autoPlay
+                    muted
+                    >
+                     <source src={sample} type="video/mp4"/>
+                    </video>
+                    <div className="videoText">
+                        <span className="leftText">
+                            Coding is&nbsp;
+                        </span>
+                        <TypeWriter 
+                            onInit={(typewriter)=>{
+                                typewriter
+                                    .typeString('easy')
+                                    .callFunction(()=>{
+                                    
+                                    })
+                                    .pauseFor(3000)
+                                    .deleteAll()
+                                    .typeString('a journey')
+                                    .deleteAll()
+                                    .start() ;        
+                            }}  
+                            options={{
+                                autoStart: true,
+                                loop: true,
+                            }}
+
+                        />
+                     </div>
+                </div>
+            </section>
+        </div>
+    )
+}
