7193b55c6b0c6934e7f7e7d2c6586187d49c61e7
Sheray <sheray@localhost.localdomain>
2021-08-02 12:02:42 +0500

    updates

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Homepage from './components/Homepage';
 import About from './components/About';
 import Education from './components/Education';
 import Contact from './components/Contact';
+import Projects from './components/Projects';
 
 import HashLoader from "react-spinners/HashLoader";
 import Fade from 'react-reveal/Fade';
@@ -50,6 +51,7 @@ function App() {
               <Homepage className={showHome ? 'display-none' : ''}/>
               <About/>
               <Education/>
+              <Projects/>
               <Contact/>
              </div>
              }
