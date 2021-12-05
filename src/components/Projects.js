022afcc987aff3f35154e85f3740902137e2968f
unknown <zeeshansheray1@gmail.com>
2021-12-05 12:41:18 +0500

    changes

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,12 +10,20 @@ export default function Projects() {
 
     const projects = [
         {
-            title : 'Snap That Home',
-            detail: 'Snap that home is an Australian based property sale/purchase website.',
-            websiteLink: 'https://snapthathome.com.au/',
+            title : 'Walletly',
+            detail: 'Wallelty is a SAAS, that offer Reward platform to Marketing Agencies and Business. Currently we are working on Version 2.0, which will be released shortly.',
+            websiteLink: 'https://walletly.ai/old_version/',
             githubLink: '',
             image: '/images/snapthathome.png',
-            active: false,
+            active: false,   
+        },
+        {
+            title : 'Vertex Wealth ',
+            detail: 'Vertex is a wealth management websities that helps you make better financial decisions. ',
+            websiteLink: 'https://vertexm.com.au/',
+            githubLink: '',
+            image: '/images/snapthathome.png',
+            active: false,   
         },
         {
             title : 'Future Money Club',
@@ -26,11 +34,11 @@ export default function Projects() {
             active: false,   
         },
         {
-            title : 'Apex Financial Planning',
-            detail: 'Apex Financial Planning was established to help everyday Australians to take control of their retirement planning and savings.',
-            websiteLink: 'https://apexfinancialplanning.com.au/',
-            githubLink: 'https://github.com/zeeshansheray/ApexFinancialPlanning',
-            image: '/images/apex.png',
+            title : 'Snap That Home',
+            detail: 'Snap that home is an Australian based property sale/purchase website.',
+            websiteLink: 'https://snapthathome.com.au/',
+            githubLink: '',
+            image: '/images/snapthathome.png',
             active: false,
         },
         {
@@ -47,6 +55,14 @@ export default function Projects() {
             websiteLink: 'https://www.beasty.app/',
             githubLink: '',
             image: '/images/beasty.png',
+            active: false,
+        },
+        {
+            title : 'Apex Financial Planning',
+            detail: 'Apex Financial Planning was established to help everyday Australians to take control of their retirement planning and savings.',
+            websiteLink: 'https://apexfinancialplanning.com.au/',
+            githubLink: 'https://github.com/zeeshansheray/ApexFinancialPlanning',
+            image: '/images/apex.png',
             active: false,
         },
     ]
